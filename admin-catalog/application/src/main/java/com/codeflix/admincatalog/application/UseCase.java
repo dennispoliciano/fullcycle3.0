df2a872252b289bb5b798267bce861b038e9c461
Dennis <denniscsp.dev@gmail.com>
2022-12-02 22:55:02 -0300

    adicionar agregado category

--- admin-catalog/application/src/main/java/com/codeflix/admincatalog/application/UseCase.java
+++ admin-catalog/application/src/main/java/com/codeflix/admincatalog/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.codeflix.admincatalog.application;
 
-import com.codeflix.admincatalog.domain.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
--- admin-catalog/domain/src/main/java/com/codeflix/admincatalog/domain/Category.java
+++
@@ -1,5 +0,0 @@
-package com.codeflix.admincatalog.domain;
-
-public class Category {
-    public String id;
-}
